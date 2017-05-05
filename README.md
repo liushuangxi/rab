7235dd0cbffbf40e2ae4df3ff1ed5cfaed261dad
liushuangxi <729021170@163.com>
2017-05-05 22:33:18 +0800

    配置修改

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 ## [参考](https://github.com/rabbitmq/rabbitmq-perf-test/blob/master/html/README.md)
 ## [依赖](https://github.com/rabbitmq/rabbitmq-perf-test/releases)
+## 使用
+### 启动server
+<pre>
+php scripts/server.php
+</pre>
+### 启动testing
+<pre>
+php scripts/testing.php
+
+注:性能测试比较耗时,大约几分钟
+</pre>
--- scripts/testing.php
+++ scripts/testing.php
@@ -7,29 +7,39 @@
 $rootDir = dirname(__DIR__) . "/";
 
 //config文件检查
-$config = isset($argv[1]) ? trim($argv[1]) : '';
-if (empty($config)) {
+$configName = isset($argv[1]) ? trim($argv[1]) : '';
+if (empty($configName)) {
     echo "config不能为空; 例:demo(对应configs/demo.js)\n";
+    showConfigs($rootDir . "configs/");
     exit;
 }
 
-$config = $rootDir . "configs/" . $config . ".js";
+$config = $rootDir . "configs/" . str_replace('.js', '', $configName) . ".js";
 if (!file_exists($config)) {
     echo "config不存在; 例:demo(对应configs/demo.js)\n";
+    showConfigs($rootDir . "configs/");
     exit;
 }
 
 //输出文件
-$output_name = date("YmdHis");
-$output = $rootDir . "framework/html/data/" . $output_name . ".js";
+$outputName = date("YmdHis");
+$output = $rootDir . "framework/html/data/" . $outputName . ".js";
 
 //执行命令
 $command = $rootDir . "framework/bin/runjava com.rabbitmq.perf.PerfTestMulti $config $output";
 
 @exec($command);
 
-echo "http://localhost:8080/output/consume.html?js_file=" . $output_name."\n";
-echo "http://localhost:8080/output/message-sizes-and-producers.html?js_file=" . $output_name."\n";
-echo "http://localhost:8080/output/message-sizes-large.html?js_file=" . $output_name."\n";
-echo "http://localhost:8080/output/rate-vs-latency.html?js_file=" . $output_name."\n";
+$url = "http://localhost:8080/output/show.html?";
+$url .= "js_file=$outputName&category=$configName";
 
+echo $url . "\n";
+
+function showConfigs($path)
+{
+    echo "\n可选参数:\n";
+    $list = glob($path . "*.js");
+    foreach ($list as $v) {
+        echo str_replace($path, '    ', str_replace('.js', '', $v)) . "\n";
+    }
+}
